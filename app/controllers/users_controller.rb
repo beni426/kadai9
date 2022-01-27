95e3eb189801f97cb9ff6546e0494f2f4e96be37
Ryu <goskn88006@yahoo.co.jp>
2022-01-27 11:02:22 +0900

    ログアウト機能を実装’

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    skip_before_action :login_required,only: [:new, :create]
     def new
         @user= User.new
     end
@@ -19,6 +20,7 @@ class UsersController < ApplicationController
     end
     def set_user
         @user = User.find(params[:id])
+    end
 end
 
 
