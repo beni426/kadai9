0731b5c9c08edec1918c51f18ce4c1c677662911
Ryu <goskn88006@yahoo.co.jp>
2022-01-27 11:29:49 +0900

    お気に入り機能を作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_053855) do
+ActiveRecord::Schema.define(version: 2022_01_27_022636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "blog_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["blog_id"], name: "index_favorites_on_blog_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -25,4 +43,7 @@ ActiveRecord::Schema.define(version: 2022_01_26_053855) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "blogs", "users"
+  add_foreign_key "favorites", "blogs"
+  add_foreign_key "favorites", "users"
 end
