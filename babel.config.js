e4a83795a72ee5f12f8adbdb7aba5a0be129aae1
Ryu <goskn88006@yahoo.co.jp>
2022-02-06 13:00:27 +0900

    babel.config.js を変更

--- babel.config.js
+++ babel.config.js
@@ -68,7 +68,7 @@ module.exports = function(api) {
         }
       ]
       
-      ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
+      ["@babel/plugin-proposal-private-methods", { "loose": true }]
     ].filter(Boolean)
   }
 }
