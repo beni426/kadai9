21a0cde8074032fd9715c2a118bb07b7ff932e8a
Ryu <goskn88006@yahoo.co.jp>
2022-02-06 13:56:02 +0900

    babel.file

--- babel.config.js
+++ babel.config.js
@@ -48,6 +48,7 @@ module.exports = function(api) {
         }
       ],
       ["@babel/plugin-proposal-private-methods", { "loose": true }],
+      ["@babel/plugin-proposal-private-property-in-object", { "loose": true }],
       [
         '@babel/plugin-proposal-object-rest-spread',
         {
@@ -68,8 +69,6 @@ module.exports = function(api) {
           async: false
         }
       ]
-      
-      ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
-    ].filter(Boolean)
+      ].filter(Boolean)
   }
 }
