f9d09d82d02eafeba5143ad87a1195959ec7fa4b
Ryu <goskn88006@yahoo.co.jp>
2022-02-08 19:29:40 +0900

    課題を編集

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,5 +6,5 @@ class FavoritesController < ApplicationController
   def destroy
      favorite = current_user.favorites.find_by(id: params[:id]).destroy
      redirect_to pictures_path, notice: "#{favorite.picture.user.name}さんのブログをお気に入り解除しました"
-  send
+  end
 end
